80f5536b5087e24bcc54c4cdc631aa39017e4e1d
shaimaa <shaimaa@sulfah.com>
2024-09-23 17:05:58 +0300

    tasklist firestore backend

--- routes/_middleware.dart
+++ routes/_middleware.dart
@@ -1,6 +1,10 @@
 import 'package:dart_frog/dart_frog.dart';
+import 'package:tasklist_backend/items/item_Repository.dart';
+import 'package:tasklist_backend/lists/list_Reository.dart';
 
 Handler middleware(Handler handler) {
-  return handler.use(requestLogger())
-                .use(provider<String>((context)=> 'Dart Frog tutorials 2023!'));
+  return handler
+      .use(requestLogger())
+      .use(provider<TaskListRepository>((context)=> TaskListRepository()))
+      .use(provider<TaskItemRepository>((context)=> TaskItemRepository()));
 }
---
+++ routes/db/fbase/index.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:dart_frog/dart_frog.dart';
+import 'package:firedart/firedart.dart';
+
+Future<Response> onRequest(RequestContext context) {
+  
+   return switch(context.request.method){
+     
+     HttpMethod.get => _getLists(context),
+    
+     HttpMethod.post => _createList(context),
+        
+     _ => Future.value(Response(statusCode: HttpStatus.methodNotAllowed))
+   };
+}
+
+Future<Response> _getLists(RequestContext context) async{
+  final lists = <Map<String, dynamic>>[];
+  await Firestore.instance.collection('tasklist').get().then((docs){
+    for(final doc in docs) {
+        lists.add(doc.map);
+    }
+  });
+  return Response.json(body: lists.toString());
+}
+
+Future<Response> _createList(RequestContext context) async{
+  final body = await context.request.json() as Map<String, dynamic>;
+  final name = body['name'] as String?;
+  final list = {'name': name};
+  final id = await Firestore.instance.collection('tasklist').add(list).then((doc){
+    return doc.id;
+  });
+  return Response.json(body: {'id': id});
+}
